dc9b05261a1a3f3c7463d963737b7441e4334b23
Allan Dutra Feitosa <allandutra@radinfo.com.br>
2022-12-23 11:58:26 -0300

    chore: add mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 
 import "../styles/global.scss";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#404eed",
+    },
+    success: {
+      main: "#1AAE9F",
+    },
+  },
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
