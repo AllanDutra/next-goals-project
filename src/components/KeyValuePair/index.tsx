4897d8858ef2afe94f8e26e6c9da31071ea9a48f
Allan Dutra Feitosa <allandutra@radinfo.com.br>
2022-12-23 11:13:12 -0300

    feat: KeyValuePair component

---
+++ src/components/KeyValuePair/index.tsx
@@ -0,0 +1,46 @@
+import { IconContext } from "phosphor-react";
+import { ReactNode } from "react";
+
+import styles from "./styles.module.scss";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+function Container({ children }: ContainerProps) {
+  return <div className={styles.container}>{children}</div>;
+}
+
+interface LabelProps {
+  labelData: string;
+  icon?: ReactNode;
+}
+
+function Label({ labelData, icon }: LabelProps) {
+  return (
+    <div className={styles.label}>
+      {icon && (
+        <IconContext.Provider
+          value={{
+            size: 20,
+            weight: "regular",
+          }}
+        >
+          {icon}
+        </IconContext.Provider>
+      )}
+
+      <span>{labelData}</span>
+    </div>
+  );
+}
+
+interface ValueProps {
+  valueData: ReactNode;
+}
+
+function Value({ valueData }: ValueProps) {
+  return <span>{valueData}</span>;
+}
+
+export const KeyValuePair = { Container, Label, Value };
