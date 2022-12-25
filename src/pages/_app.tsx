f6b801cf80c078c9d197ebb99aeec8dfb2ac4584
allandutra <allandutrafeitosa14@gmail.com>
2022-12-25 18:36:41 -0300

    feat: add react toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 import "../styles/global.scss";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 moment.locale("pt-br");
 
@@ -28,6 +30,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <NextAuthProvider session={pageProps.session}>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          newestOnTop
+          closeOnClick
+          pauseOnFocusLoss={false}
+          pauseOnHover
+          theme="dark"
+        />
       </ThemeProvider>
     </NextAuthProvider>
   );
